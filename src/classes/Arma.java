9768d003cc5fb76604b5a41223ed56c032fd68f5
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-29 16:04:21 -0300

    Implementando Tipo da arma com final

--- src/classes/Arma.java
+++ src/classes/Arma.java
@@ -1,6 +1,25 @@
 package classes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Arma {
+  public static final Integer FUZIL = 1;
+  public static final Integer RIFLE = 2;
+  public static final Integer SUBMETRALHADORA = 3;
+  public static final Integer ARMASPESADAS = 4;
+  public static final Integer PISTOLA = 5;
+
+  public static Map<Integer, String> Tipos = new HashMap<>() {
+    {
+      put(FUZIL, "Fuzil");
+      put(RIFLE, "Rifle");
+      put(SUBMETRALHADORA, "Submetralhadora");
+      put(ARMASPESADAS, "Armas pesadas");
+      put(PISTOLA, "Pistola");
+    }
+  };
+
   private String tipo;
   private String nome;
   private String cor;
